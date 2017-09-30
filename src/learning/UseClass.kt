6ab1fe9cbcb60af5b28b9b90a5b398bbd06d657d
ibisha <akashiroakaaka@gmail.com>
2017-09-30 16:12:43 +0900

    Expansion function

--- src/learning/UseClass.kt
+++ src/learning/UseClass.kt
@@ -6,11 +6,19 @@ class IntPair(val first: Int, val second: Int) {
     fun swap(): IntPair = IntPair(second, first)
 }
 
+fun Int.meet(n: Int): IntPair {
+    return IntPair(this, n)
+}
+
 fun main(args: Array<String>) {
     val ip: IntPair = IntPair(3, 5)
     println("Sum : ${ip.sum()}") // => 8
     println("Max : ${ip.max()}") // => 5
     println("Swap : ${ip.swap().first}, ${ip.swap().second}") // => 5,3
+
+    println("----Int.meet----")
+    val meet: IntPair = 9.meet(5)
+    println("${meet.first}, ${meet.second}")
 
 }
 
